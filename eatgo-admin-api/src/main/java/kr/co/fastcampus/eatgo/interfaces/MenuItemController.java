5735a00361d1df238af11a4b59fc78afe858f9d7
rkdls8268 <rkdls8268@gmail.com>
2020-08-27 17:50:35 +0900

    [영속화] h2 file db, getMenuItems 추가

--- eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/MenuItemController.java
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/MenuItemController.java
@@ -3,10 +3,7 @@ package kr.co.fastcampus.eatgo.interfaces;
 import kr.co.fastcampus.eatgo.application.MenuItemService;
 import kr.co.fastcampus.eatgo.domain.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +13,11 @@ public class MenuItemController {
 
     @Autowired
     private MenuItemService menuItemService;
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems")
+    public List<MenuItem> getMenuItems(@PathVariable("restaurantId") Long restaurantId) {
+        return menuItemService.getMenuItems(restaurantId);
+    }
 
     @PatchMapping("/restaurants/{restaurantId}/menuitems")
     public String bulkUpdate(
